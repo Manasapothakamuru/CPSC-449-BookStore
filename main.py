58ee1c0b62449953d506d8855a3d789354eab78a
Naveen Golla-123 <naveengolla123@gmail.com>
2023-05-15 14:21:45 -0700

    Changes made on main.py

--- main.py
+++ main.py
@@ -3,10 +3,39 @@ from pymongo import MongoClient, ASCENDING
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from bson.objectid import ObjectId
+from pymongo.errors import WriteError
 
 app = FastAPI()
 client = MongoClient("mongodb://localhost:27017/")
 db = client["bookStore"]
+
+result = db.command({
+    "collMod": "books",
+    "validator": {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["title", "author"],
+            "properties": {
+                "title": {
+                    "bsonType": "string"
+                },
+                "author": {
+                    "bsonType": "string"
+                },
+                "price": {
+                "type": "number",
+                "minimum": 0,
+                "maximum": 1000,
+                "exclusiveMaximum": False,
+                "exclusiveMinimum": False
+                }
+            }
+        }
+    },
+    "validationLevel": "strict",
+    "validationAction": "error"
+})
+
 collection = db["books"]
 
 class Book(BaseModel):
@@ -16,6 +45,7 @@ class Book(BaseModel):
     description: str
     price: int
     stock: int
+    sold_items: int
 
 class SearchPayload(BaseModel):
     searchBy : str
@@ -23,16 +53,6 @@ class SearchPayload(BaseModel):
     min: str
     max: str
 
-# collection.create_index([('title', 'text')])
-
-# collection.create_index([('author', 'text')])
-
-# collection.create_index([('price', ASCENDING)])
-
-# collection.create_index([('stock', ASCENDING)])
-
-
-# Get all books
 @app.get("/books")
 def index():
     result = collection.find()
@@ -42,7 +62,6 @@ def index():
         res.append(i)
     return JSONResponse(content=res)
 
-#Get book by Id
 @app.get("/book/{book_id}")
 async def getbookById(book_id:str):
     result = collection.find_one({"_id": ObjectId(book_id)})
@@ -51,11 +70,14 @@ async def getbookById(book_id:str):
         result["_id"] = str(result["_id"])
     return result
 
-@app.post("/users")
+@app.post("/book")
 def create_user(book: Book):
     print(book)
     book_dict = book.dict()
-    collection.insert_one(book_dict) # to be checked failure 
+    try:
+        collection.insert_one(book_dict) # to be checked failure 
+    except WriteError as w:
+        return f"Saving record failed {w}"
     return True
 
 @app.put("/")
@@ -65,10 +87,14 @@ def updateBook(book:Book):
     book_id = book_dict["book_id"]
     book_dict.pop("book_id", None)
     book_oid = ObjectId(book_id)
-    result = collection.update_one(
-        {"_id": book_oid},
-        {"$set": book_dict}
-    )
+
+    try:
+        result = collection.update_one(
+            {"_id": book_oid},
+            {"$set": book_dict}
+        )
+    except WriteError as w:
+        print(f"Update book failed {w}")
 
     if result.modified_count == 1:
         return {"message": "Book updated successfully"}
@@ -102,10 +128,10 @@ def search(searchPayload: SearchPayload):
         res.append(i)
     return JSONResponse(content=res)
 
-@app.get("/books/top-authors")
+@app.get("/books/top5authors")
 def get_top_authors():
     pipeline = [
-        {"$group": {"_id": "$author", "count": {"$sum": 1}}},
+        {"$group": {"_id": "$author", "count": {"$sum": 2}}},
         {"$sort": {"count": -1}},
         {"$limit": 5}
     ]
@@ -114,6 +140,38 @@ def get_top_authors():
     for doc in result:
         authors.append(doc['_id'])
     return {"authors": authors}
+
+@app.get("/books/top-selling")
+async def get_top_selling_books():
+    pipeline = [
+        {"$group": {"_id": "$title", "total_sold": {"$sum": "$sold_items"}}},
+        {"$sort": {"total_sold": 1}},
+        {"$limit": 5}
+    ]
+    result = collection.aggregate(pipeline)
+    books = []
+    for doc in result:
+        books.append(doc['_id'])
+    return {"books": books}
+
+@app.get("/books/count")
+async def get_books_count():
+    count = collection.count_documents({})
+    return {"count": count}
+
+@app.post("/books/purchase/{book_id}")
+def purchaseBook(book_id: str):
+    query = {"_id": ObjectId(book_id)}
+    update = {"$inc": {"sold_items": 1, "stock": -1}}
+    try:
+        collection.update_one(query, update)
+    except WriteError as e:
+        print(f"Purchase failed {e}")
+        return False
+    return True
+
+
+
    
     
 
